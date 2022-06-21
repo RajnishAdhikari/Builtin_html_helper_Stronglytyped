bad8f0f5134f4d61d0bd9c3aa685699a6eb8168c
Rajnish Adhikari <rajnishadhikari01@gmail.com>
2022-06-21 20:37:34 +0545

    Add project files.

---
+++ Builtin_html_helper_Stronglytyped/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Builtin_html_helper_Stronglytyped.Models;
+
+namespace Builtin_html_helper_Stronglytyped.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        { 
+            Student s1 = new Student()
+            {
+                Id = 10000,
+                Name = "ram",
+                Age = 20,
+                Address = "Nayabazar",
+                IsPass = true,
+            };
+          return View(s1);
+
+        }
+
+    }
+}
---
+++ Builtin_html_helper_Stronglytyped/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Builtin_html_helper_Stronglytyped.MvcApplication" Language="C#" %>
